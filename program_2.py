71a6ae0e0e3ba4d27faf85f0398f8144af0d29f1
javangraber-design <javangraber@gmail.com>
2026-02-14 12:03:12 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,12 +1,42 @@
+# Programmer: Javan Graber
+# Date: February 14, 2026
 # Program #2: Movie Tix
-# Write a program that has the user input various movie names and how many 
-# tickets are desired for each movie.  
-# At the end of the program it prints out the total number of tickets desired by the user.  
+# Write a program that has the user input various movie names and how many
+# tickets are desired for each movie.
+# At the end of the program it prints out the total number of tickets desired by the user.
 # Use either a "for loop" or "while loop" to accomplish this.
 
 def main():
     ######################
-    # WRITE YOUR CODE HERE
+    # Establish accumulator
+    total = 0
+
+    # Establish the Boolean expression
+    another_movie = True
+
+    while another_movie == True:
+        # Get the movie
+        film = input('Enter a movie title: ')
+        # Get the tickets
+        tickets = int(input(f'Enter the number of tickets for {film}: '))
+        # Add the tickets to the total
+        total += tickets
+
+        # Make a validation loop
+        if tickets < 0:
+            print('Error! That is invalid.')
+            tickets = int(input(f'Enter the real number of tickets for {film}: '))
+
+        # Ask for another movie
+        question = input('Is there another movie (type y or n)? ')
+        if question == 'y':
+            another_movie = True
+        else:
+            another_movie = False
+
+    # Display the total
+    print(f'You need to purchase {total} tickets.')
+
     ######################
 
 
